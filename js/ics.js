93d7efc660c0f312fb24fbae5d1ed19394e92bec
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-29 14:45:51 +0000

    Deploy JavaBy/JavaBy.github.io to github.com/JavaBy/JavaBy.github.io.git:master

---
+++ js/ics.js
@@ -0,0 +1,114 @@
+/* global saveAs, Blob, BlobBuilder, console */
+/* exported ics */
+
+var ics = function() {
+    'use strict';
+
+    if (navigator.userAgent.indexOf('MSIE') > -1 && navigator.userAgent.indexOf('MSIE 10') == -1) {
+        console.log('Unsupported Browser');
+        return;
+    }
+
+    var SEPARATOR = (navigator.appVersion.indexOf('Win') !== -1) ? '\r\n' : '\n';
+    var calendarEvents = [];
+    var calendarStart = [
+        'BEGIN:VCALENDAR',
+        'VERSION:2.0'
+    ].join(SEPARATOR);
+    var calendarEnd = SEPARATOR + 'END:VCALENDAR';
+
+    return {
+        /**
+         * Returns events array
+         * @return {array} Events
+         */
+        'events': function() {
+            return calendarEvents;
+        },
+
+        /**
+         * Returns calendar
+         * @return {string} Calendar in iCalendar format
+         */
+        'calendar': function() {
+            return calendarStart + SEPARATOR + calendarEvents.join(SEPARATOR) + calendarEnd;
+        },
+
+        /**
+         * Add event to the calendar
+         * @param  {string} subject     Subject/Title of event
+         * @param  {string} description Description of event
+         * @param  {string} location    Location of event
+         * @param  {string} begin       Beginning date of event
+         * @param  {string} end        Ending date of event
+         */
+        'addEvent': function(subject, description, location, begin, end) {
+            // I'm not in the mood to make these optional... So they are all required
+            if (typeof subject === 'undefined' ||
+                typeof description === 'undefined' ||
+                typeof location === 'undefined' ||
+                typeof begin === 'undefined' ||
+                typeof end === 'undefined'
+            ) {
+                return false;
+            };
+
+            var calendarEvent = [
+                'BEGIN:VEVENT',
+                'CLASS:PUBLIC',
+                'DESCRIPTION:' + description,
+                'DTSTART;' + begin,
+                'DTEND;' + end,
+                'LOCATION:' + location,
+                'SUMMARY;LANGUAGE=en-us:' + subject,
+                'TRANSP:TRANSPARENT',
+                'END:VEVENT'
+            ].join(SEPARATOR);
+
+            calendarEvents.push(calendarEvent);
+            return calendarEvent;
+        },
+
+        /**
+         * Download calendar using the saveAs function from filesave.js
+         * @param  {string} filename Filename
+         * @param  {string} ext      Extention
+         */
+        'download': function(filename, ext) {
+            if (calendarEvents.length < 1) {
+                return false;
+            }
+
+            ext = (typeof ext !== 'undefined') ? ext : '.ics';
+            filename = (typeof filename !== 'undefined') ? filename : 'calendar';
+            var calendar = calendarStart + SEPARATOR + calendarEvents.join(SEPARATOR) + calendarEnd;
+
+            var blob;
+            if (navigator.userAgent.indexOf('MSIE 10') === -1) { // chrome or firefox
+                blob = new Blob([calendar]);
+            } else { // ie
+                var bb = new BlobBuilder();
+                bb.append(calendar);
+                blob = bb.getBlob('text/x-vCalendar;charset=' + document.characterSet);
+            }
+            saveAs(blob, filename + ext);
+            return calendar;
+        }
+    };
+};
+
+function ical(event) {
+  event.beginTimezone = typeof event.beginTimezone !== 'undefined' ? event.beginTimezone : 'Europe/Minsk';
+  event.beginTimezone = typeof event.beginTimezone !== 'undefined' ? event.endTimezone : event.beginTimezone;
+
+  var ical = ics();
+
+  ical.addEvent(
+    event.subject,
+    event.description,
+    event.location,
+    "TZID=" + event.beginTimezone + ":" + event.begin,
+    "TZID=" + event.endTimezone + ":" + event.end
+  );
+  ical.download(event.subject);
+}
